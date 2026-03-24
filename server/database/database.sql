5a9c80cedd159fd7d2de45f491979d0b55ac811e
mats <172794@student.horizoncollege.nl>
2026-03-24 12:09:00 +0100

    database chanches

--- server/database/database.sql
+++ server/database/database.sql
@@ -5,7 +5,7 @@ USE `middenbeemster_Smidse`;
 
 CREATE TABLE UserAdmin (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
@@ -18,31 +18,8 @@ CREATE TABLE Content (
     PublishedBy varchar(255) NOT NULL,
     lastEditedBy varchar(255),
     CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UpdatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    UpdatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (PublishedBy) REFERENCES UserAdmin(username),
+    FOREIGN KEY (lastEditedBy) REFERENCES UserAdmin(username)
 );
-
-FOREIGN KEY (PublishedBy) REFERENCES UserAdmin(username),
-FOREIGN KEY (lastEditedBy) REFERENCES UserAdmin(username);CREATE DATABASE `middenbeemster_Smidse`;
-
-USE `middenbeemster_Smidse`;
-
-CREATE TABLE UserAdmin (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE Content (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    Location VARCHAR(255) NOT NULL,
-    ApiName VARCHAR(255) NOT NULL,
-    Content TEXT NOT NULL,
-    Type ENUM('text', 'image', 'video') NOT NULL,
-    PublishedBy varchar(255) NOT NULL,
-    lastEditedBy varchar(255),
-    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UpdatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-FOREIGN KEY (PublishedBy) REFERENCES UserAdmin(username),
-FOREIGN KEY (lastEditedBy) REFERENCES UserAdmin(username);
