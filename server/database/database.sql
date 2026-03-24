740c3459be1900cc227b7c7c5ec6a490e60bd9bc
mats <172794@student.horizoncollege.nl>
2026-03-24 11:40:23 +0100

    database update

--- server/database/database.sql
+++ server/database/database.sql
@@ -1,23 +1,47 @@
+-- DROP DATABASE IF EXISTS `middenbeemster_Smidse`;
 CREATE DATABASE `middenbeemster_Smidse`;
 
 USE `middenbeemster_Smidse`;
 
 CREATE TABLE UserAdmin (
-    'id' INT PRIMARY KEY AUTO_INCREMENT,
-    'username' VARCHAR(255) NOT NULL,
-    'password' VARCHAR(255) NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Content (
-    'id' INT PRIMARY KEY AUTO_INCREMENT,
-    'Location' VARCHAR(255) NOT NULL,
-    'ApiName' VARCHAR(255) NOT NULL,
-    'Content' TEXT NOT NULL,
-    'Type' ENUM('text', 'image', 'video') NOT NULL,
-    'PublishedBy' varchar(255) NOT NULL,
-    'lastEditedBy' varchar(255),
-    'CreatedAt' TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    'UpdatedAt' TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    Location VARCHAR(255) NOT NULL,
+    ApiName VARCHAR(255) NOT NULL,
+    Content TEXT NOT NULL,
+    Type ENUM('text', 'image', 'video') NOT NULL,
+    PublishedBy varchar(255) NOT NULL,
+    lastEditedBy varchar(255),
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UpdatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+FOREIGN KEY (PublishedBy) REFERENCES UserAdmin(username),
+FOREIGN KEY (lastEditedBy) REFERENCES UserAdmin(username);CREATE DATABASE `middenbeemster_Smidse`;
+
+USE `middenbeemster_Smidse`;
+
+CREATE TABLE UserAdmin (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Content (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    Location VARCHAR(255) NOT NULL,
+    ApiName VARCHAR(255) NOT NULL,
+    Content TEXT NOT NULL,
+    Type ENUM('text', 'image', 'video') NOT NULL,
+    PublishedBy varchar(255) NOT NULL,
+    lastEditedBy varchar(255),
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UpdatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 FOREIGN KEY (PublishedBy) REFERENCES UserAdmin(username),
