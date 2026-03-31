cfba70fb919734127df59ff95259b8033edd71ac
Aryan Lachman <aryanlachman07@gmail.com>
2026-03-31 12:16:34 +0200

    fork1

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
+  plugins: [vue()],
+  base: 'middenbeemster-xgrid-design',
 })
